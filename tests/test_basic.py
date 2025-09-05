af424f62d941e2e48152e3d158457d67864e4671
JoeDestructor <joancamilojcvs@gmail.com>
2025-09-05 12:19:04 -0500

    probando workflow

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -15,4 +15,4 @@ def test_home_page():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Welcome" in response.data  # Ajusta según tu texto en Jinja
+    assert b"Temporizador Pomodoro" in response.data  # Ajusta según tu texto en Jinja
